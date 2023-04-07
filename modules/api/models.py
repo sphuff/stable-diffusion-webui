f92793bc5726093b7a8c674922cabff09143f9a7
Stephen Huffnagle <stephen@stephens-mbp.lan>
2023-04-07 10:02:36 -0400

    fix

--- modules/api/models.py
+++ modules/api/models.py
@@ -106,8 +106,8 @@ StableDiffusionTxt2ImgProcessingAPI = PydanticModelGenerator(
         {"key": "script_args", "type": list, "default": []},
         {"key": "send_images", "type": bool, "default": True},
         {"key": "save_images", "type": bool, "default": False},
-        {"key": "alwayson_scripts", "type": dict, "default": {}}
-        {"key": "model_hash", "type": str, "default": None, "exclude" : True},
+        {"key": "alwayson_scripts", "type": dict, "default": {}},
+        {"key": "model_hash", "type": str, "default": None, "exclude" : True}
     ]
 ).generate_model()
 
@@ -124,8 +124,8 @@ StableDiffusionImg2ImgProcessingAPI = PydanticModelGenerator(
         {"key": "script_args", "type": list, "default": []},
         {"key": "send_images", "type": bool, "default": True},
         {"key": "save_images", "type": bool, "default": False},
-        {"key": "alwayson_scripts", "type": dict, "default": {}}
-        {"key": "model_hash", "type": str, "default": None,  "exclude" : True},
+        {"key": "alwayson_scripts", "type": dict, "default": {}},
+        {"key": "model_hash", "type": str, "default": None,  "exclude" : True}
     ]
 ).generate_model()
 
